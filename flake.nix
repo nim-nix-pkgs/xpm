f1a76243af164c260a4a0c526642281b4e775a2e
hugosenari <hugosenari@gmail.com>
2022-03-12 01:54:52 -0300

    chore: re index xpm

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''X-Pixmap & NetPBM'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."xpm-master".url = "path:./master";
+  inputs."xpm-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."xpm-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
